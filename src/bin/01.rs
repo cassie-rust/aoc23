32f0425d89e97351f0082f9221ca4f757c6f118f
Cassandra S. <sbrvhy4z@duck.com>
2023-12-01 10:03:31 +0100

    Day 1 done

---
+++ src/bin/01.rs
@@ -0,0 +1,53 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(
+        input
+            .lines()
+            .map(|l| l.chars().filter(|c| c.is_digit(10)).collect::<Vec<char>>())
+            .map(|d| {
+                let mut s = String::new();
+                s.push(*d.first().unwrap());
+                s.push(*d.last().unwrap());
+                s.parse::<u32>().unwrap()
+            })
+            .sum::<u32>(),
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let res = input
+        .lines()
+        .map(|v| {
+            v.replace("one", "o1ne")
+                .replace("two", "t2wo")
+                .replace("three", "t3hree")
+                .replace("four", "f4our")
+                .replace("five", "f5ive")
+                .replace("six", "s6ix")
+                .replace("seven", "s7even")
+                .replace("eight", "e8ight")
+                .replace("nine", "n9ine")
+        })
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    part_one(&res)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
